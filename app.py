e31e80ad6771b3e0ce139ddb6772fb7d954c9f5d
santospedroh <santos.pedroh@gmail.com>
2021-09-22 22:05:11 -0300

    connection python to mysql

--- app.py
+++ app.py
@@ -1,49 +1,53 @@
 from flask import Flask, render_template, request
-#from flask_wtf.csrf import CSRFProtect
-import sqlite3
-
 app = Flask(__name__)
-#csrf = CSRFProtect(app)
+
+# variaveis de conexao MySQL
+
 
 def consult():
     # conexao
-    conn = sqlite3.connect('post-it-database.db')
-    conn.row_factory = sqlite3.Row
-    curr = conn.cursor()
+    curr = conn.connection.cursor()
+    sql = "SELECT nome, mensagem, datetime(criado_em,'localtime') as criado_em FROM post ORDER BY criado_em DESC"
+
     # consulta
-    curr.execute("SELECT nome, mensagem, datetime(criado_em,'localtime') as criado_em FROM post ORDER BY criado_em DESC")
+    curr.execute(sql)
     rows = curr.fetchall()
     return rows
 
-def save(nome, mensagem):
+
+# def save(nome, mensagem):
     # conexao
-    conn = sqlite3.connect('post-it-database.db')
-    curr = conn.cursor()  
-    #salva
-    curr.execute("INSERT into post (nome, mensagem) values (?,?)",(nome,mensagem))  
-    conn.commit()  
+    #conn = sqlite3.connect('post-it-database.db')
+    #curr = conn.cursor()
+    # salva
+    # curr.execute("INSERT into post (nome, mensagem) values (?,?)",
+    #             (nome, mensagem))
+    # conn.commit()
+
 
 @app.route("/")
 def home():
     try:
         rows = consult()
-        print("Efetuando consulta no SQLite.") 
-        return render_template('index.html', posts = rows, show_modal=False )
+        print("Efetuando consulta no SQLite.")
+        return render_template('index.html', posts=rows, show_modal=False)
     except Exception as e:
         print(e)
 
-@app.route("/salvar", methods = ["POST","GET"])
+
+@app.route("/salvar", methods=["POST", "GET"])
 def salvar():
-     if request.method == "POST":
+    if request.method == "POST":
         try:
-            name = request.form["nome"]  
-            message = request.form["mensagem"]  
-            save(name, message)                     
+            name = request.form["nome"]
+            message = request.form["mensagem"]
+            #save(name, message)
             rows = consult()
-            print("Salvando dados no SQLite.")   
-            return render_template('index.html', posts = rows, show_modal=True)         
-        except Exception as e: 
+            print("Salvando dados no SQLite.")
+            return render_template('index.html', posts=rows, show_modal=True)
+        except Exception as e:
             print(e)
 
+
 if __name__ == '__main__':
-    app.run(debug = True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
